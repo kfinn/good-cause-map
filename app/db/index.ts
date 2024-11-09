368a2f946cdae7476d85a269fd91037adc3a4231
Kevin Finn <superkevin@gmail.com>
2024-11-09 00:28:33 -0500

    log the database url for now

--- app/db/index.ts
+++ app/db/index.ts
@@ -1,6 +1,7 @@
 import postgres from "postgres";
 
 export default function connect() {
+  console.log(process.env.DATABASE_URL);
   return postgres(
     process.env.DATABASE_URL ??
       "postgresql://nycdb:nycdb@localhost:54321/nycdb",
